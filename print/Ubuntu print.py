c2cbca370dade04d8f10d00a95059c14763f1e7b
DogeIlMeme <DogeIlMeme@protonmail.com>
2020-03-27 16:11:37 +0100

    Create Ubuntu print.py
    I created a very simple program to print two variables on the screen.

---
+++ print/Ubuntu print.py
@@ -0,0 +1,3 @@
+Ubuntu = "An operating system"
+Linux = "is system software that manages computer hardware, software resources, and provides common services for computer programs."
+print(Ubuntu,Linux)
